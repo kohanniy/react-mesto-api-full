158f4ae53da23e0734875885d843fabb7f6784f4
Вячеслав Коханный <xoxluwa1@yandex.com>
2021-04-24 20:45:56 +0500

    hotfix: backend - использовать без маршрутов

--- backend/app.js
+++ backend/app.js
@@ -40,7 +40,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger);
-app.use('*', cors(options));
+app.use(cors(options));
 app.post('/signup', checkNewUser, createUser);
 app.post('/signin', checkLogin, login);
 
